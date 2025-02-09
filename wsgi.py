f22f5da84ac6f8422e4b4f3630c0473a726121ae
Wachiraphat Kaewmalang <131470936+KIRoNE41@users.noreply.github.com>
2025-02-09 23:28:35 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))  # Get the port from environment or use 5000 for local
+    port = int(os.environ.get("PORT", 5000))  # Render จะตั้งค่าตัวแปร PORT ให้อยู่แล้ว
     app.run(host="0.0.0.0", port=port)
